18459e8a362f4b162f7aa138bdf13e91ecc15436
Takenori Sato <takenori.sato@gmail.com>
2018-02-16 13:04:21 +0900

    removed TenguCounter and TenguReporter

--- tengu/__init__.py
+++ tengu/__init__.py
@@ -4,5 +4,3 @@ from .tengu_flow_analyzer import TenguNode, TenguFlowAnalyzer, TenguFlowNode, Te
 from .tengu_scene_analyzer import TenguSceneAnalyzer
 from .tengu_detector import TenguDetector, TenguBackgroundSubtractionDetector
 from .tengu_tracker import TenguTracker, TenguCostMatrix, Tracklet
-from .tengu_counter import TenguCounter, TenguObsoleteCounter
-from .tengu_count_reporter import TenguCountReporter, TenguTotalAggregator, TenguFormatter, TenguConsoleWriter, TenguFpsAggregator, TenguNumberListToCSVFormatter, TenguFileWriter
